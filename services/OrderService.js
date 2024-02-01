891801f80ab70f0fc2940e8d61cec1f208cc2a3c
elhaj-lachgar <143610909+elhaj-lachgar@users.noreply.github.com>
2024-02-01 18:56:31 +0100

    connected

--- services/OrderService.js
+++ services/OrderService.js
@@ -1,9 +1,15 @@
+const { PrismaClient } = require("@prisma/client");
 const expressAsyncHandler = require("express-async-handler");
-const prisma = require('../utils/PrismaClient')
+// const prisma = require('../utils/PrismaClient')
+const prisma = new PrismaClient ();
 
 exports.CreateOrderService = expressAsyncHandler(async (req, res, next) => {
 const order = await prisma.order.create({
-    data: { ...req.body.object },
+    data: {
+        addressId : req.body.object.addressId,
+        cardId : req.body.object.cardId ,
+        userId : req.body.object.userId,
+    }
   });
   return res.status(200).json({ data: order });
 });
