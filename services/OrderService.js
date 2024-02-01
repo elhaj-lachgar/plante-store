aed523412b7bf8dd86bcdb601ba3ecf59e6331c9
elhaj-lachgar <143610909+elhaj-lachgar@users.noreply.github.com>
2024-02-01 15:03:15 +0100

    handle create or

--- services/OrderService.js
+++ services/OrderService.js
@@ -4,7 +4,7 @@ const prisma = require("../utils/PrismaClient");
 exports.CreateOrderService = expressAsyncHandler(async (req, res, next) => {
   console.log(req.body.object);
   const order = await prisma.order.create({
-    data: {DelaiverdAt:null , isDelaiverd : false , ...req.body.object },
+    data: {isDelaiverdAt:null , isDelaiverd : false , ...req.body.object },
   });
   return res.status(200).json({ data: order });
 });
